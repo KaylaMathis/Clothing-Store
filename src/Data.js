f4fd16b06977540c914091d76ef82d74d4d3f71d
Swampskeleton9 <kamathis42@gmail.com>
2023-05-17 11:19:52 -0500

    message

---
+++ src/Data.js
@@ -0,0 +1,86 @@
+let shopItemData = [
+    {
+        id: "lsdj",
+        name: "Black Dress",
+        price: 45,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/black.jpg"
+    },
+    {
+        id: "sdf",
+        name: "Blue Dress",
+        price: 100,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/blue.jpg"
+    }, 
+    {
+        id: "aeswer",
+        name: "Checker Dress",
+        price: 25,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/checker.jpg"
+    }, 
+    {
+        id: "cvklji",
+        name: "Brown Dress",
+        price: 300,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/brown.jpg"
+    },
+    {
+        id: "asdf",
+        name: "Pink Dress",
+        price: 150,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/pink.jpg"
+    },
+    {
+        id: "werwe",
+        name: "Red Dress",
+        price: 200,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/red.jpg"
+    },
+    {
+        id: "poiu",
+        name: "Floral Dress",
+        price: 50,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/floral.jpg"
+    },
+    {
+        id: "akjld",
+        name: "White Dress",
+        price: 75,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/white.jpg"
+    },
+    {
+        id: "mirlk",
+        name: "Green Dress",
+        price: 100,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/green.jpg"
+    },
+    {
+        id: "uyi",
+        name: "Silver Dress",
+        price: 400,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/silver.jpg"
+    },
+    {
+        id: "liu",
+        name: "Black and Red Dress",
+        price: 50,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/black-red.jpg"
+    },
+    {
+        id: "ytr",
+        name: "Rainbow Dress",
+        price: 200,
+        desc: "Lorem ipsum dolor sit amet consectetur adipisicing.",
+        img: "images/rainbow.jpg"
+    },
+];
